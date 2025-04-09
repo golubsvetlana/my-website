44e5f1ed2881c4fccd097500ad799e13de0c33e5
SvGOl <sv.golub88@gmail.com>
2025-04-09 07:25:32 -0600

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,11 @@ export default function Home() {
           <div className="flex flex-col">
             <span className="flex flex-wrap text-justify">
               <p className="mb-3">Hi! I&apos;m Svitlana Holub, a passionate Junior Front-End Developer with a strong foundation in HTML, CSS, JavaScript, and React.</p>
-              <p className="mb-3">I love building clean, responsive, and user-friendly web applications that enhance user experience.</p>
+              {/* <p className="mb-3">I love building clean, responsive, and user-friendly web applications that enhance user experience.</p> */}
               <p className="mb-3">I&apos;m always eager to learn new technologies and improve my coding skills. Currently, I&apos;m exploring Next.js framework to create more dynamic and interactive web solutions.</p>  
-              <p>Let&apos;s connect and bring ideas to life through code!</p>
+              <p className="mb-3">Let&apos;s connect and bring ideas to life through code!</p>
+              <p className="font-semibold">This site is still under construction, and responsive design will be implemented soon to enhance the user experience across all devices.</p>  
+
             </span>
             <div className="btn-wrap"> {/*flex justify-around m-5*/}
               <Link className="btn font-semibold border-2 border-solid border-black rounded-xl hover:bg-black hover:text-white" href={"/Svitlana_Holub_Resume_Dev.pdf"} download='Svitlana_Holub_Resume_Dev.pdf'  target='_blank'>Resume</Link>
